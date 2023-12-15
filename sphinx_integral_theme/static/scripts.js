70f8214ea0f729c6583e8074a622594df46d9b50
Roughtron <roughtron@yandex.ru>
2023-12-15 21:25:01 +0100

    change theme

--- sphinx_integral_theme/static/scripts.js
+++ sphinx_integral_theme/static/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var caption = "#left-menu p.caption";
+  var caption = "#pytorch-left-menu p.caption";
   var collapseAdded = $(this).not("checked");
   var collapsedSections = [];
   var openArrow = '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 10L8 6.12121L4 2" stroke="#8D93A1" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
